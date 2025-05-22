5105723cf0e5e8c4c30944bd83e8b7dea8bcd0c3
KrisMih <kristianmih15@gmail.com>
2025-05-22 22:10:05 +0300

    Create basic comment creation

--- groupnest/comments/views.py
+++ groupnest/comments/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ValidationError
+from .models import Comment
+from posts.models import Post
+from serializers import CommentSerializer
 
-# Create your views here.
+class CommentListCreateView(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        post_id = self.kwargs.get("post_id")
+        try:
+            post = Post.objects.get(id = post_id)
+        except Post.DoesNotExist:
+            raise ValidationError({"error": "Post not found"})
+        return Comment.objects.filter(post = post)
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get('post_id')
+        try:
+            post = Post.objects.get(id = post_id)
+        except Post.DoesNotExist:
+            raise ValidationError({"error": "Post not found"})
+        serializer.save(author = self.request.user, post = post)
