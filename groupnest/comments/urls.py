fbb494c704972e09b3ff505256227bd35e9cc086
KrisMih <kristianmih15@gmail.com>
2025-06-11 18:11:34 +0300

    change comment url

--- groupnest/comments/urls.py
+++ groupnest/comments/urls.py
@@ -3,5 +3,5 @@ from .views import CommentListCreateView, CommentRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('posts/<int:post_id>/comments/', CommentListCreateView.as_view(), name = 'comment-list-create'),
-    path('comments/<int:pk>/', CommentRetrieveUpdateDestroyView.as_view(), name = 'comment-retrieve-update-delete'),
+    path('comments/<int:pk>/deleteupdate/', CommentRetrieveUpdateDestroyView.as_view(), name = 'comment-retrieve-update-delete'),
 ]
